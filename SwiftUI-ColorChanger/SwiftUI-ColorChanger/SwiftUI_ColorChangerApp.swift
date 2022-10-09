4083a69e34425e126b378b003f8c561ad037b6a7
VasckMe <kasarin.anton@gmail.com>
2022-10-09 19:28:32 +0300

    Project created

---
+++ SwiftUI-ColorChanger/SwiftUI-ColorChanger/SwiftUI_ColorChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ColorChangerApp.swift
+//  SwiftUI-ColorChanger
+//
+//  Created by Apple Macbook Pro 13 on 9.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ColorChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
